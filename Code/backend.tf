221dd76f6f2475960ccb26ccd2a6cd0e98fe484a
Navin <124908644+Navinalone@users.noreply.github.com>
2023-02-14 20:31:56 +0530

    Update backend.tf

--- Code/backend.tf
+++ Code/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "testnavin1"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
-    access_key           = "4rE9Jc6d/dPzIOQsDJPmL69pZ4OM3vRNlvrAUKOGLoJOfbDLPjIMYQrHs4svDATtP9rwCx4K7VO3+AStIctCCw=="
+    access_key           = ""
   }
 }
